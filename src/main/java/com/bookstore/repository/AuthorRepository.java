b07ebcf1fa49d0262175f44a6edfaeb7a89d97a8
Osy <ikuenobeosi@gmail.com>
2023-12-27 07:51:02 +0100

    Modified search author query to include penName

--- src/main/java/com/bookstore/repository/AuthorRepository.java
+++ src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -9,7 +9,7 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-    @Query("SELECT u FROM Author u WHERE firstName LIKE %:name% OR lastName LIKE %:name%")
+    @Query("SELECT u FROM Author u WHERE firstName LIKE %:name% OR lastName LIKE %:name% OR penName LIKE %:name%")
     Collection<Author> searchAuthorByName(@Param("name") String name);
 
     Collection<Author> findAllByFirstNameOrLastName(String firstName, String lastName);
